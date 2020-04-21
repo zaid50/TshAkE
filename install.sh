fdcf39878986be8a5e77983b6e9b88e7c3603bd9
TshAkEAb <64094722+TshAkEAb@users.noreply.github.com>
2020-04-21 15:53:45 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ chmod +x ts
 get() {
 rm -fr Tshake.lua
 rm -fr sudo.lua
-wget "https://github.com/TshAkEAb/TshAkE/tree/master/Tshake.lua"
+wget "https://raw.githubusercontent.com/Tshakeab/Tshake/master/Tshake.lua"
 lua start.lua
 }
 installall(){
